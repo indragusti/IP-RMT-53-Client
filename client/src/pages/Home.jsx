dd22bfefbfc6ec871eeb1a408ab16d630ee0324b
indragusti <gustianto.indra18@gmail.com>
2024-10-01 22:27:33 +0700

    feat: add login page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,59 @@
-// import { useState, useEffect } from "react";
-// import { baseURL } from "../helpers/baseUrl";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { baseURL } from "../helpers/baseUrl";
+import MonsterCard from "../components/Card";
 
 export default function Home() {
+  const [monsters, setMonsters] = useState([]);
+  const navigate = useNavigate();
+
+  const fetchMonsters = async () => {
+    try {
+      const response = await baseURL.get("/monster", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        },
+      });
+
+      console.log(response.data, "<<< fetchMonsters");
+      setMonsters(response.data.data);
+    } catch (err) {
+      // localStorage.removeItem("access_token");
+      // navigate("/login");
+      console.log(err, "<<< err fetchMonsters");
+    }
+  };
+
+  useEffect(() => {
+    fetchMonsters();
+  }, []);
+
   return (
     <>
-      <div></div>
+      <div className="container mt-4">
+        {/* <div className="mb-4">
+          <button
+            onClick={() => navigate("/favorites")}
+            className="btn btn-primary me-2"
+          >
+            See your favorite monsters
+          </button>
+        </div> */}
+        <h2 className="mb-4">Monster List</h2>
+
+        <div className="d-flex flex-wrap justify-content-between">
+          {monsters.map((e) => (
+            <MonsterCard
+              key={e.id}
+              monster={{
+                id: e.id,
+                name: e.name,
+                Image: e.Image.imageUrl,
+              }}
+            />
+          ))}
+        </div>
+      </div>
     </>
   );
 }
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,110 @@
-// import { useState, useEffect } from "react";
-// import { baseURL } from "../helpers/baseUrl";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { baseURL } from "../helpers/baseUrl";
 
 export default function Login() {
+  const [email, setEmail] = useState("admin@mail.com");
+  const [password, setPassword] = useState("admin1234");
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await baseURL.post("/login", {
+        email,
+        password,
+      });
+      localStorage.setItem("access_token", response.data.access_token);
+      localStorage.setItem("userId", response.data.userId);
+      console.log(response.data, "<<< handleLogin");
+      navigate("/");
+    } catch (err) {
+      console.log(err, "<<< handleLogin");
+    }
+  };
+
+  // function handleCredentialResponse(response) {
+  //   console.log("Encoded JWT ID token: " + response.credential);
+  // }
+  // const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
+  // useEffect(() => {
+  //   window.google.accounts.id.initialize({
+  //     client_id: googleClientId,
+  //     callback: handleCredentialResponse,
+  //   });
+  //   window.google.accounts.id.renderButton(
+  //     document.getElementById("buttonDiv"),
+  //     {
+  //       theme: "outline",
+  //       size: "large",
+  //     }
+  //   );
+  //   // google.accounts.id.prompt();
+  // }, []);
+
   return (
-    <>
-      <div></div>
-    </>
+    <div className="flex items-center justify-center h-screen ">
+      <div className="bg-gray-800 rounded-lg shadow-lg max-w-sm w-full">
+        <h3 className="text-lg font-semibold text-center p-4 border-b border-red-600 text-white">
+          Login
+        </h3>
+        <div className="p-6">
+          <form onSubmit={handleLogin}>
+            <div className="mb-4">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-300"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                className="mt-1 block w-full p-2 bg-gray-700 text-white border border-gray-600 rounded-md focus:outline-none focus:ring focus:ring-red-500"
+                id="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-300"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                className="mt-1 block w-full p-2 bg-gray-700 text-white border border-gray-600 rounded-md focus:outline-none focus:ring focus:ring-red-500"
+                id="password"
+                placeholder="Enter password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-red-600 text-white font-semibold py-2 rounded-md hover:bg-red-700 transition duration-200"
+            >
+              Submit
+            </button>
+          </form>
+          <div className="flex justify-center mt-4">
+            <button
+              type="button"
+              className="w-full bg-gray-600 text-gray-200 font-semibold py-2 rounded-md hover:bg-gray-500 transition duration-200"
+              onClick={() => navigate("/register")}
+            >
+              Register
+            </button>
+          </div>
+          <div className="flex justify-center mt-3">
+            <div id="buttonDiv"></div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
