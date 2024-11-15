64d8c955faac4f0cb09a1ab6ae9c91a2f489d64b
indragusti <gustianto.indra18@gmail.com>
2024-11-15 11:38:49 +0700

    fix: google auth

--- client/src/helpers/baseUrl.js
+++ client/src/helpers/baseUrl.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const baseURL = axios.create({
-  baseURL: "http://localhost:3000",
-  // baseURL: "https://lion.indragusti.my.id",
+  // baseURL: "http://localhost:3000",
+  baseURL: "https://wolf.indragusti.my.id",
 });
